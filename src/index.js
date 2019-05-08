ab411270572b6f339809b2f53a4db56d2e7a9bd9
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-08 15:56:28 -0500

    working on adding

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
+const $ = require('jQuery');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  $('#loading').html(`<p id=loading>Here are all the movies:</p>`);
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('#loading').append(`<ul>
+<li>id#${id} - ${title} - rating: ${rating}</li>
+</ul>`)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
+});
+
+$('#subBttn').click(function(){
+  let movieTitle = $('#movieTitle').val();
+  let rating = $("#rating").val();
+ const movie = {
+   "title": movieTitle,
+    "rating": rating
+  };
+ const url = '/api/movies';
+  const newMovie = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+  };
+  console.log(movie);
+  fetch(url,movie)
+      .then(console.log("done"))
+      .catch(console.log("error"))
 });
