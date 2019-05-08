e9b5a3be2c686a67f1e212ce4bd0f9f10fb2deb5
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-08 16:46:02 -0500

    we can now add

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@
  */
 const $ = require('jQuery');
 const {getMovies} = require('./api.js');
-
-getMovies().then((movies) => {
-  $('#loading').html(`<p id=loading>Here are all the movies:</p>`);
-  movies.forEach(({title, rating, id}) => {
-    $('#loading').append(`<ul>
+const updateMovies = (()=>{
+  getMovies().then((movies) => {
+    $('#loading').html(`<p id=loading>Here are all the movies:</p>`);
+    movies.forEach(({title, rating, id}) => {
+      $('#loading').append(`<ul>
 <li>id#${id} - ${title} - rating: ${rating}</li>
 </ul>`)
+    });
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
   });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+
+
 });
 
 $('#subBttn').click(function(){
@@ -29,16 +32,7 @@ $('#subBttn').click(function(){
    "title": movieTitle,
     "rating": rating
   };
- const url = '/api/movies';
-  const newMovie = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(movie)
-  };
-  console.log(movie);
-  fetch(url,movie)
-      .then(console.log("done"))
-      .catch(console.log("error"))
+ $.post('/api/movies', movie);
+ updateMovies();
 });
+updateMovies();
