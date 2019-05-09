176e3368aa0f71a70051ae1d3e2be21b01fa6b95
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-09 14:14:24 -0500

    Everything but bounus is complete

--- src/index.js
+++ src/index.js
@@ -9,69 +9,87 @@
  */
 const $ = require('jQuery');
 const {getMovies} = require('./api.js');
-const updateMovies = (()=>{
-  getMovies().then((movies) => {
-    $('#loading').html(`<p id=loading>Here are all the movies:</p>`);
-    movies.forEach(({title, rating, id}) => {
-      $('#loading').append(`<ul>
+const updateMovies = (() => {
+    $('#loading').html(`<p id="loading">Loading...</p>`)
+    getMovies().then((movies) => {
+        $('#loading').html(`<p id=loading>Here are all the movies:</p>`);
+        movies.forEach(({title, rating, id}) => {
+            $('#loading').append(`<ul>
 <li>id#${id} - ${title} - rating: ${rating}</li>
-</ul>`)})
-  }).then(()=>{
-    buildDropDown();
-  })
-      .catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
-  });
+<button type="submit" value=${id} class="del">Delete</button>
+</ul>`)
+        })
+    }).then(() => {
+        buildDropDown();
+    })
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
+        });
 });
 const buildDropDown = (() => {
-  $('#movieSelect').empty();
-  getMovies().then((movies) => {
-    movies.forEach(({title, rating, id}) => {
-      $('#movieSelect').append(`<option value="${title}${id}${rating}">${title} ${rating}</option>`)
+    $('#movieSelect').empty();
+    getMovies().then((movies) => {
+        movies.forEach(({title, rating, id}) => {
+            $('#movieSelect').append(`<option value="${title}${id}${rating}">${title} ${rating}</option>`)
+        })
     })
-  })
 });
-$('#edit').click(function(){
-  let movieToEdit = $('#movieSelect').val();
-  console.log(movieToEdit)
-  let ratingToEdit = movieToEdit.substring(movieToEdit.length - 1, movieToEdit.length);
-  movieToEdit = movieToEdit.substring(0, movieToEdit.length - 2 );
+$('#edit').click(function() {
+    let movieToEdit = $('#movieSelect').val();
+    console.log(movieToEdit)
+    let ratingToEdit = movieToEdit.substring(movieToEdit.length - 1, movieToEdit.length);
+    movieToEdit = movieToEdit.substring(0, movieToEdit.length - 2);
 
-  console.log(movieToEdit);
-  console.log(ratingToEdit);
-  $('#editForm').toggleClass("visible");
-  $('#newMovieTitle').replaceWith(`<input type="text" id="newMovieTitle" name="newMovieTitle" placeholder=${movieToEdit}>`)
-  $('#newRating').replaceWith(`    <input type="text" id="newRating" name="newRating" placeholder=${ratingToEdit}>`)
+
+    console.log(movieToEdit);
+    console.log(ratingToEdit);
+    $('#editForm').toggleClass("visible");
+    document.getElementById("newMovieTitle").value = movieToEdit;
+    // $('#newMovieTitle').replaceWith(`<input type="text" id="newMovieTitle" name="newMovieTitle" value=${movieToEdit}>`);
+    $('#newRating').replaceWith(`<input type="text" id="newRating" name="newRating" value=${ratingToEdit}>`)
 });
 
-$('#subBttn').click(function(){
-  let movieTitle = $('#movieTitle').val();
-  let rating = $("#rating").val();
- const movie = {
-   "title": movieTitle,
-    "rating": rating
-  };
- $.post('/api/movies', movie);
- updateMovies();
+$('#subBttn').click(function () {
+    let movieTitle = $('#movieTitle').val();
+    let rating = $("#rating").val();
+    const movie = {
+        "title": movieTitle,
+        "rating": rating
+    };
+    $.post('/api/movies', movie);
+    updateMovies();
 });
 updateMovies();
 
-$('#submitEdit').click(function(e){
-  e.preventDefault();
-  let movieTitle = $('#newMovieTitle').val();
-  let rating = $("#newRating").val();
-  let id = $('#movieSelect').val();
-  id = id.substring(id.length - 2, id.length - 1);
-  console.log(id);
-  const movie = {
-    "title": movieTitle,
-    "rating": rating
-  };
-  $.ajax(`/api/movies/${id}`, {
-    method:'PATCH',
-    data: JSON.stringify(movie),
-   contentType: 'application/json'
-  });
-updateMovies();
+$('#submitEdit').click(function (e) {
+    e.preventDefault();
+    let movieTitle = $('#newMovieTitle').val();
+    let rating = $("#newRating").val();
+    let id = $('#movieSelect').val();
+    id = id.substring(id.length - 2, id.length - 1);
+    console.log(id);
+    const movie = {
+        "title": movieTitle,
+        "rating": rating
+    };
+    $.ajax(`/api/movies/${id}`, {
+        method: 'PATCH',
+        data: JSON.stringify(movie),
+        contentType: 'application/json'
+    });
+    updateMovies();
 });
+
+
+$(document).on('click', '.del', function (e) {
+    // e.preventDefault();
+    let id = $(this).val();
+    console.log(id);
+    $.ajax(`/api/movies/${id}`, {
+        method: 'DELETE',
+        contentType: 'application/json'
+    });
+    updateMovies();
+});
+
